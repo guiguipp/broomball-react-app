1dda08a4dd4e09d86234fa78c40f10c46408322b
Guillaume Paugam <guipaugam@icloud.com>
2018-09-13 16:54:22 -0500

    Heroku deploy

--- server.js
+++ server.js
@@ -54,7 +54,8 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
     // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname + '/client/build/index.html'));
+        console.log("req.params in server.js: ", req.params)
+        res.sendFile(path.join(__dirname + "/client/build/", 'index.html'));
     });
     }
     
