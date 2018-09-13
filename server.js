52936de547227e8c643871591f3550f096b1f889
Guillaume Paugam <guipaugam@icloud.com>
2018-09-13 16:21:27 -0500

    Heroku Deploy

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const mongoose = require("mongoose");
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
 // CORS issues
 const cors = require('cors')
@@ -47,16 +47,16 @@ if (process.env.NODE_ENV === 'production') {
 // =============================================================
 const routes = require("./routes")
 app.use(routes);
-/*
+
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use(express.static('client/build'));
     // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
+    // app.get('*', function(req, res) {
+    //     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    // });
     }
-*/
+    
 app.listen(process.env.PORT || 8080, function(){
 console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
