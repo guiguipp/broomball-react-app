321eb5c0267c763af0e89ff57627c593abfe78c6
Guillaume Paugam <35872384+guiguipp@users.noreply.github.com>
2018-09-13 22:49:28 -0500

    Update API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 // Transforming the current url in order to query the public API locally 
 // (Express port 3001 changed to React port 3000 where the API exists).
 // Once deployed, it should reset to the domain name used.
-const currentURL = "http://localhost:3000" ? "http://localhost:8080" : window.location.origin
+const currentURL = window.location.origin
 
 export default {
     getGames: () => {       
